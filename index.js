9d6ffcfd10e43aafba715b1da9a2451b4a9aeb95
NyantasticUwU <NyantasticUwU@gmail.com>
2020-07-28 03:49:13 -0500

    Added projects section

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ $(window).on("load", function () {
     $("#LoadingScreen").fadeOut("slow");
     $("#LandingPage").fadeIn("slow");
 });
+
+// Landing page
+function openGithubPage() {
+    window.open("https://github.com/NyantasticUwU", "_blank");
+}
